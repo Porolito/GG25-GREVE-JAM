f28c5b54857a3c46728da0393c82724022728eae
notpatern <rodiersasha@gmail.com>
2025-02-03 18:44:39 +0000

    made style points so the game is fun

---
+++ TENCENT SIMULATOR (Riot)/Assets/Scripts/SO_DataStyle.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+class SO_DataStyle : ScriptableObject {
+    int nearMiss;
+    int closeSave;
+    int save;
+    int brotherChill;
+
+    int niceSort;
+    int greatSort;
+    int blazinglyFastSort;
+    int missedSort;
+
+    int disturbingThePeace;
+    int ouch;
+    int maybleLookAtTheTutorial;
+    int okYoureDeffoAfk;
+}
---
+++ TENCENT SIMULATOR (Riot)/Assets/Scripts/StylePoint.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class StylePoint : MonoBehaviour
+{
+    int m_score;
+    [SerializeField] ScriptableObject m_styleData;
+
+    void Start() {
+        m_score = 0;
+    }
+
+    void Update() {
+
+    }
+}
