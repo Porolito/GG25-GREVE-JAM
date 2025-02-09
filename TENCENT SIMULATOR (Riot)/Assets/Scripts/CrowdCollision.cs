d9904bc2a37cb33d256e4e0e13669c6b8ca0832d
Porolito <pierre.thomas16380@gmail.com>
2025-02-09 18:08:49 +0100

    final caca push pt prout

--- TENCENT SIMULATOR (Riot)/Assets/Scripts/CrowdCollision.cs
+++ TENCENT SIMULATOR (Riot)/Assets/Scripts/CrowdCollision.cs
@@ -4,10 +4,14 @@ public class CrowdCollision : MonoBehaviour
 {
     [SerializeField] private CrowdMovement m_crowdMovement;
 
+    [SerializeField] private PlayerScore m_PlayerScore;
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.transform.CompareTag("Lacrym") || collider.transform.CompareTag("Donut") & collider.GetComponent<ProjectileBehavior>().m_isMoving == true)
         {
+            m_PlayerScore.BreakCombo();
+            m_PlayerScore.MinusScore(100);
             m_crowdMovement.HandleEnemyBounceBack();
         }
     }
